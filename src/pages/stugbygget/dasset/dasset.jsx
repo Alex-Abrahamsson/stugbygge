c3b2de2a6473bd575ff189988a344fda3611ffcd
Alex <Alex.abrahamsson@gmail.com>
2022-06-22 09:49:29 +0200

    test

--- src/pages/stugbygget/dasset/dasset.jsx
+++ src/pages/stugbygget/dasset/dasset.jsx
@@ -12,7 +12,7 @@ function Dasset() {
       <ImageGallery
         additionalClass={styles.MyGallery}
         disableSwipe={false}
-        autoPlay={true} // set false or comment if donnot want to auto play
+        autoPlay={true} // set false orr comment if donnot want to auto play
         showFullscreenButton={true} // set false to hide button
         showPlayButton={true} // set false to hide button
         items={dassImages}
